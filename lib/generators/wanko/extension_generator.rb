b2b82427627601e069f35cdc53cc1e2cdc7a94b2
Akira Matsuda <ronnie@dio.jp>
2015-10-13 15:46:01 +0900

    Unneeded files for new extensions

--- lib/generators/wanko/extension_generator.rb
+++ lib/generators/wanko/extension_generator.rb
@@ -1,6 +1,11 @@
 require 'rails/generators/rails/plugin/plugin_generator'
 
 module Wanko
+  class ExtensionBuilder < ::Rails::PluginBuilder
+    def readme() end
+    def rakefile() end
+  end
+
   module Generators
     class ExtensionGenerator < ::Rails::Generators::PluginGenerator
       source_root ::Rails::Generators::PluginGenerator.source_root
@@ -18,6 +23,20 @@ module Wanko
         options = @options.dup
         options[:mountable] = options[:skip_bundle] = options[:skip_test_unit] = options[:skip_git] = options[:skip_gemfile] = true
         @options = options.freeze
+      end
+
+      def get_builder_class
+        Wanko::ExtensionBuilder
+      end
+
+      # override
+      def create_bin_files
+      end
+
+      def put_litter_in_its_place
+        remove_file 'MIT-LICENSE'
+        remove_file "app/controllers/#{name}/application_controller.rb"
+        remove_file "lib/tasks/#{name}_tasks.rake"
       end
     end
   end
