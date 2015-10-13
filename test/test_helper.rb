d5a43caa60526e01ec8579f9cf1830b935b7c635
Akira Matsuda <ronnie@dio.jp>
2015-10-13 14:43:06 +0900

    Dummy Rails app for testing the engine

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,21 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'wanko'
+# Configure Rails Environment
+ENV["RAILS_ENV"] = "test"
 
-require 'minitest/autorun'
+require File.expand_path("../../test/dummy_app/config/environment.rb",  __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy_app/db/migrate", __FILE__)]
+ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
+require "rails/test_help"
+
+# Filter out Minitest backtrace while allowing backtrace from other libraries
+# to be shown.
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+  ActiveSupport::TestCase.fixtures :all
+end
