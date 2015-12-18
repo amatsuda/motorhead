1e61ab4362962c4a6500c083129efa302901848f
Akira Matsuda <ronnie@dio.jp>
2015-12-18 15:18:18 +0900

    raise from within the generated default active_if block
    Defaulting to "always true" might be too optimistic, and possibly cause some serious security issue.

--- lib/generators/motorhead/templates/lib/%name%/engine.rb
+++ lib/generators/motorhead/templates/lib/%name%/engine.rb
@@ -4,6 +4,15 @@ module <%= camelized %>
   class Engine < ::Rails::Engine
     include Motorhead::Engine
 
+    active_if { raise <<-MSG }
+Please configure appropriate access control for <%= camelized %> Engine.
+
+e.g.
+1) accessible for the admin users only:
+    active_if { current_user.admin? }
+
+2) always enabled for everyone:
     active_if { true }
+MSG
   end
 end
