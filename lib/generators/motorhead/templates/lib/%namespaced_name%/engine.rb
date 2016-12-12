aec9346b0be4717877e9e87cb7d67fe45c6f09d8
Akira Matsuda <ronnie@dio.jp>
2016-12-12 21:18:56 +0900

    Rails 5 requires lib/%namespaced_name%/engine.rb template for the engine generator
    https://github.com/rails/rails/commit/ee9e4c340e

---
+++ lib/generators/motorhead/templates/lib/%namespaced_name%/engine.rb
@@ -0,0 +1,18 @@
+require 'motorhead/engine'
+
+module <%= camelized %>
+  class Engine < ::Rails::Engine
+    include Motorhead::Engine
+
+    active_if { raise Motorhead::ActiveIfNotConfigured, <<-MSG }
+Please configure appropriate access control for <%= camelized %> Engine.
+
+e.g.
+1) accessible for the admin users only:
+    active_if { current_user.admin? }
+
+2) always enabled for everyone:
+    active_if { true }
+MSG
+  end
+end
